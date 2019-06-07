0842ef291a6f6de346220b8b27c3c89da8a0270a
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 23:03:50 -0300

    Add ui and tamanho on state of redux

--- amaro/src/redux-flow/reducers/index.js
+++ amaro/src/redux-flow/reducers/index.js
@@ -5,10 +5,12 @@ import produtos from './produtos/index'
 import filter from './filter-produtos/index'
 import carrinho from './carrinho/index'
 import ui from './ui/index'
+import tamanhos from './tamanhos'
 
 export default combineReducers({
   produtos,
   filter,
   carrinho,
-  ui
+  ui,
+  tamanhos
 })
