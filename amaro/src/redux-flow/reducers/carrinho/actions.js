30a37e869c57775de77dbdb0248c1888be1024a2
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 23:06:41 -0300

    Add actions to increment, decrement and delete produto car

--- amaro/src/redux-flow/reducers/carrinho/actions.js
+++ amaro/src/redux-flow/reducers/carrinho/actions.js
@@ -1,3 +1,7 @@
 'use strict'
 
-export const ADD_PRODUTO_CARRINHO = 'addProdutoCarrinho:ADD_PRODUTO_CARRINHO'
+export const ADD_PRODUTO_CARRINHO = 'produtoCarrinho:ADD_PRODUTO_CARRINHO'
+export const INCREMENT_QNTD = 'produtoCarrinho:INCREMENT_QNTD'
+export const DECREMENT_QNTD = 'produtoCarrinho:DECREMENT_QNTD'
+export const DELETE_PRODUTO_CARRINHO = 'produtoCarrinho:DELETE_PRODUTO_CARRINHO'
+
