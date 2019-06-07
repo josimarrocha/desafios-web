9ab8634c41ea7894664273bc2ec5bf1a9787d244
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 23:04:55 -0300

    Add method increment, decrement, deleteProduto

--- amaro/src/redux-flow/reducers/carrinho/action-creators.js
+++ amaro/src/redux-flow/reducers/carrinho/action-creators.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { ADD_PRODUTO_CARRINHO } from './actions'
+import { ADD_PRODUTO_CARRINHO, INCREMENT_QNTD, DECREMENT_QNTD, DELETE_PRODUTO_CARRINHO } from './actions'
 
 export const addCarrinho = ({ nome, code, imagem, preco_atual }, tamanhos) => ({
   type: ADD_PRODUTO_CARRINHO,
@@ -9,7 +9,28 @@ export const addCarrinho = ({ nome, code, imagem, preco_atual }, tamanhos) => ({
     imagem,
     nome,
     preco_atual,
-    qntd: tamanhos.length,
-    sizes: tamanhos
+    qntd: tamanhos.filter(item => item.code === code).length,
+    sizes: tamanhos.filter(item => item.code === code)
+  }
+})
+
+export const incrementQntd = (code) => ({
+  type: INCREMENT_QNTD,
+  payload: {
+    code
+  }
+})
+
+export const decrementQntd = (code) => ({
+  type: DECREMENT_QNTD,
+  payload: {
+    code
+  }
+})
+
+export const deleteProdutoCarrinho = (code) => ({
+  type: DELETE_PRODUTO_CARRINHO,
+  payload: {
+    code
   }
 })
