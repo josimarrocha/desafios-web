3ae9347cba9cdf63e072ece6ba9812a7e327a406
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 23:08:22 -0300

    Add functions to delete, increment and decrement on state

--- amaro/src/redux-flow/reducers/carrinho/index.js
+++ amaro/src/redux-flow/reducers/carrinho/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { ADD_PRODUTO_CARRINHO } from './actions'
+import { ADD_PRODUTO_CARRINHO, INCREMENT_QNTD, DECREMENT_QNTD, DELETE_PRODUTO_CARRINHO } from './actions'
 import createReducer from 'reducers/createReducer'
 
 const initialState = {}
@@ -14,6 +14,26 @@ const carrinhoProduto = createReducer(initialState, {
       preco_atual: action.payload.preco_atual,
       qntd: action.payload.qntd,
       sizes: action.payload.sizes
+    }
+  }),
+  [DELETE_PRODUTO_CARRINHO]: (state, action) => {
+    let { [action.payload.code]: teste, ...newState } = state
+    return newState
+  },
+
+  [INCREMENT_QNTD]: (state, action) => ({
+    ...state,
+    [action.payload.code]: {
+      ...state[action.payload.code],
+      qntd: state[action.payload.code].qntd + 1
+    }
+  }),
+
+  [DECREMENT_QNTD]: (state, action) => ({
+    ...state,
+    [action.payload.code]: {
+      ...state[action.payload.code],
+      qntd: state[action.payload.code].qntd - 1
     }
   })
 })
