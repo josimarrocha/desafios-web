de09c8fe4cdfc528e5365efe3b41b48482e92f93
Josimar <rocha_josimar@yahoo.com.br>
2019-06-06 23:00:51 -0300

    Add method increment and decrement

--- amaro/src/components/carrinho.js
+++ amaro/src/components/carrinho.js
@@ -4,47 +4,73 @@ import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { closeCarrinho } from 'reducers/ui/action-creators'
+import { incrementQntd, decrementQntd, deleteProdutoCarrinho } from 'reducers/carrinho/action-creators'
 
-const Carrinho = ({ produtosCarrinho, toggleCarrinho, closeCarrinho }) => (
-  <CarrinhoContainer style={{ transform: `translateX( ${toggleCarrinho.openCarrinho ? '0%' : '101%'})` }}>
-    <Header>
-      <div className='fecha' onClick={closeCarrinho}>
-        <span />
-      </div>
-    </Header>
-    <ProdutosCarrinho>
-      <Title>Produtos adicionados ao carrinho</Title>
-      <ProdutoList className='produtos-carrinho-list'>
-        {Object.keys(produtosCarrinho).map((item, index) => (
-          <Produto key={index}>
-            <ProdutoDados>
-              <img src={produtosCarrinho[item].imagem} alt="" />
-              <div>
-                <h2>{produtosCarrinho[item].nome}</h2>
-                <Label>{produtosCarrinho[item].preco_atual}</Label><br />
-                <Label>QNTD:</Label>
-                <InputQuantidade type="text" value={produtosCarrinho[item].qntd} />
-                <span>-</span>
-                <span>+</span><br />
-                <ul>
-                  <Label>Tamanhos:</Label>
-                  {produtosCarrinho[item].sizes.map(item => (
-                    <li key={item.code}><a href="">{item.tamanho}</a></li>
-                  ))}
-                  {/* <li><a href="">M</a></li> */}
-                </ul>
-                <h2 style={{ float: 'right' }}>Total:
-                 {(produtosCarrinho[item].preco_atual.replace('R$', '').replace(',', '.') * produtosCarrinho[item].qntd)}
-                </h2>
-              </div>
-            </ProdutoDados>
-          </Produto>
-        ))}
-      </ProdutoList>
-    </ProdutosCarrinho>
-  </CarrinhoContainer>
-)
+const Carrinho = ({
+  produtosCarrinho,
+  toggleCarrinho,
+  closeCarrinho,
+  incrementQntd,
+  decrementQntd,
+  deleteProduto
+}) => (
+    <CarrinhoContainer style={{ transform: `translateX( ${toggleCarrinho.openCarrinho ? '0%' : '101%'})` }}>
+      <Header>
+        <div className='fecha' onClick={closeCarrinho}>
+          <span />
+        </div>
+      </Header>
+      <ProdutosCarrinho>
+        <Title>Produtos adicionados ao carrinho</Title>
+        <ProdutoList className='produtos-carrinho-list'>
+          {Object.keys(produtosCarrinho).map((item, index) => (
+            <Produto key={index}>
+              <ProdutoDados>
+                <DeleteProduto onClick={deleteProduto(produtosCarrinho[item].code)} />
+                <img src={produtosCarrinho[item].imagem} alt="" />
+                <div>
+                  <h2>{produtosCarrinho[item].nome}</h2>
+                  <Label>{produtosCarrinho[item].preco_atual}</Label><br />
+                  <Label>QNTD:</Label>
+                  <InputQuantidade type="text" value={produtosCarrinho[item].qntd} />
+                  <IncrementDecrement
+                    onClick={decrementQntd(produtosCarrinho[item].code)}>-
+                  </IncrementDecrement>
+                  <IncrementDecrement
+                    onClick={incrementQntd(produtosCarrinho[item].code)}>+
+                </IncrementDecrement><br />
+                  <ul>
+                    <Label>Tamanhos:</Label>
+                    {console.log(produtosCarrinho)}
+                    {produtosCarrinho[item].sizes.map((item, i) => (
+                      <li key={i}><a href="">{item.size}</a></li>
+                    ))}
+                  </ul>
+                  <h2 style={{ float: 'right' }}>Total:
+                  </h2>
+                </div>
+              </ProdutoDados>
+            </Produto>
+          ))}
+        </ProdutoList>
+      </ProdutosCarrinho>
+    </CarrinhoContainer>
+  )
+const IncrementDecrement = styled.span`
+  cursor: pointer;
+  &:active{
+    transform: translate(3%, 3%)
+  }
+`
 
+const DeleteProduto = styled.div`
+  width:24px;
+  height:24px;
+  background: red;
+  position: absolute;
+  right: 0;
+  top: 0;
+`
 
 const Header = styled.div`
   height: 60px;
@@ -65,6 +91,7 @@ const InputQuantidade = styled.input`
 `
 
 const ProdutoDados = styled.div`
+  position: relative;
   display:flex;
   img{
     align-self: flex-start;
@@ -149,6 +176,11 @@ const mapStateToProps = (state) => ({
   toggleCarrinho: state.ui
 })
 
-const mapDispatchToProps = { closeCarrinho }
+const mapDispatchToProps = (dispatch) => ({
+  closeCarrinho: () => dispatch(closeCarrinho()),
+  incrementQntd: (code) => () => dispatch(incrementQntd(code)),
+  decrementQntd: (code) => () => dispatch(decrementQntd(code)),
+  deleteProduto: (code) => () => dispatch(deleteProdutoCarrinho(code))
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Carrinho)
